4102f9c5ca593f4f4fd9b95e7a7a93c85bc5b012
Kushal001 <kushal121320@gmail.com>
2022-02-08 17:10:00 +0545

    fixed positioning problem not fixed

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,7 +1,17 @@
+// Styles
 import classes from "./Home.module.css"
 
+// Components
+import Posts from "./Posts/Posts"
+import Suggestions from "./Suggestions/Suggestions"
+
 const Home = () => {
-  return <div className={classes.home}></div>
+  return (
+    <div className={classes.home}>
+      <Posts />
+      <Suggestions />
+    </div>
+  )
 }
 
 export default Home
--- client/src/components/Home/Posts/Posts.js
+++ client/src/components/Home/Posts/Posts.js
@@ -1,7 +1,29 @@
 import classes from "./Posts.module.css"
 
 const Posts = () => {
-  return <div className={classes.posts}>Posts</div>
+  return (
+    <div className={classes.posts}>
+      <div className={classes.post}>
+        <div className={classes.testbg}></div>
+        <div>text</div>
+      </div>
+
+      <div className={classes.post}>
+        <div className={classes.testbg}></div>
+        <div>text</div>
+      </div>
+
+      <div className={classes.post}>
+        <div className={classes.testbg}></div>
+        <div>text</div>
+      </div>
+
+      <div className={classes.post}>
+        <div className={classes.testbg}></div>
+        <div>text</div>
+      </div>
+    </div>
+  )
 }
 
 export default Posts
