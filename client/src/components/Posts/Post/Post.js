3156114dde55ef8bb706a38b368599d1ba46837a
Kushal001 <kushal121320@gmail.com>
2022-02-16 22:43:16 +0545

    Posts UI except comments done

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -1,9 +1,15 @@
 import classes from "./Post.module.css"
 
-import userImage from "../../../assets/Images/post-user-profile-3.png"
-import PostImage from "../../../assets/Images/home-post-2.png"
+import userImage from "../../../assets/Images/post-user-profile-1.png"
+import PostImage from "../../../assets/Images/home-post-4.png"
 
-import { MenuIcon } from "../../../assets/svg/Svg-icons"
+import {
+  MenuIcon,
+  LikeIcon,
+  CommentIcon,
+  SendIcon,
+  BookmarkIcon,
+} from "../../../assets/svg/Svg-icons"
 
 const Post = () => {
   return (
@@ -20,7 +26,30 @@ const Post = () => {
       <div className={classes.image}>
         <img src={PostImage} alt="user" />
       </div>
-      <div className={classes.desc}>Desc</div>
+      <div className={classes.desc}>
+        <div className={classes.interactions}>
+          <div className={classes.left}>
+            <LikeIcon />
+            <CommentIcon />
+            <SendIcon />
+          </div>
+          <div className={classes.right}>
+            <BookmarkIcon />
+          </div>
+        </div>
+
+        <p className={classes["like-count"]}>25,666 Likes</p>
+
+        <div className={classes["user-desc"]}>
+          <div className={classes["username"]}>morganstanley__</div>
+          <div>newest paint in my nike af1s. say 😍 if you like it.</div>
+        </div>
+
+        {/**
+         * TODO
+         * Comment Section
+         */}
+      </div>
     </div>
   )
 }
