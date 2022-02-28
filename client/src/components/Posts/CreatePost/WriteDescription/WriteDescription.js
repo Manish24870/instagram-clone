e091f145c19ce3b8983d37366fd79d5c145696ad
Kushal001 <kushal121320@gmail.com>
2022-02-28 15:19:59 +0545

    create post - write caption text area design completed

--- client/src/components/Posts/CreatePost/WriteDescription/WriteDescription.js
+++ client/src/components/Posts/CreatePost/WriteDescription/WriteDescription.js
@@ -1,7 +1,27 @@
+import { useState } from "react"
+
 import classes from "./WriteDescription.module.css"
 
 const WriteDescription = () => {
-  return <div>Write Description</div>
+  const [wordCount, setWordCount] = useState(0)
+
+  return (
+    <div className={classes.desc}>
+      <form>
+        <div className={classes.caption}>
+          <textarea
+            id="txtarea"
+            placeholder="Write a caption..."
+            maxLength="200"
+            onChange={() => setWordCount((wordCount) => wordCount + 1)}
+          />
+          <label for="txtarea" className={classes.captionLabel}>
+            <span>{wordCount}</span>/200
+          </label>
+        </div>
+      </form>
+    </div>
+  )
 }
 
 export default WriteDescription
