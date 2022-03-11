0ee9c10654f5b739cfdda7b511dacf8d9d2e54b6
Kushal001 <kushal121320@gmail.com>
2022-03-11 17:26:18 +0545

    added crop ui

--- client/src/components/Posts/CreatePost/CropImage/CropImage.js
+++ client/src/components/Posts/CreatePost/CropImage/CropImage.js
@@ -1,17 +1,39 @@
-import { useState } from "react"
+import { useState, useCallback } from "react"
 
-import ReactCrop from "react-image-crop"
+import classes from "./CropImage.module.css"
+import Cropper from "react-easy-crop"
 
 const CropImage = ({ image }) => {
-  const [crop, setCrop] = useState({ aspect: 1 / 1 })
+  const [crop, setCrop] = useState({ x: 0, y: 0 })
+  const [zoom, setZoom] = useState(1)
+
+  const onCropComplete = useCallback((croppedArea, croppedAreaPixels) => {
+    console.log(croppedArea, croppedAreaPixels)
+  }, [])
 
   return (
-    <div>
-      <ReactCrop
-        src={image}
-        crop={crop}
-        onChange={(newCrop) => setCrop(newCrop)}
-      />
+    <div className={classes.container}>
+      <div className={classes["crop-container"]}>
+        <Cropper
+          image={image}
+          crop={crop}
+          zoom={zoom}
+          aspect={4 / 3}
+          onCropChange={setCrop}
+          onCropComplete={onCropComplete}
+          onZoomChange={setZoom}
+          classes={{
+            containerClassName: `${classes.crop}`,
+            cropAreaClassName: `${classes["crop-area"]}`,
+            mediaClassName: `${classes["media-area"]}`,
+          }}
+        />
+      </div>
+
+      {/* <div className={classes["crop-controls"]}>
+        <div>Zoom Slider</div>
+        <div>Button For completion</div>
+      </div> */}
     </div>
   )
 }
