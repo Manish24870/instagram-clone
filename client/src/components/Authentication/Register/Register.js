ef2393ad321956169a8cd093ecf1bf41278baca0
Kushal001 <kushal121320@gmail.com>
2022-03-27 15:38:46 +0545

    regx for email done

--- client/src/components/Authentication/Register/Register.js
+++ client/src/components/Authentication/Register/Register.js
@@ -13,22 +13,46 @@ import {
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
 /**
- * Starts with lower or uppercase letter
- * Followed by anywhere between 3 to 23 characters consisting of lower/upper class letter, digits, "-" or "_"
+ * Regular Expressions
+ */
+/** Username
+ *  Must start with lower or uppercase letter
+ *  Can be lower/upper class letter, digits, "-" or "_"
+ *  Can be anywhere between 3 to 23 characters
  */
 const USER_REGEX = /^[a-zA-Z][a-zA-Z0-9-_]{3,23}$/
 
-/**
- * At least
+/** Password
+ *  At least
  *  - one lower case letter
  *  - one upper case letter
  *  - one digit
  *  - one special character
- * Can be anywhere between 8 to 24 characters
+ *  Can be anywhere between 8 to 24 characters
  */
 const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/
 
+/** Email
+ *  Format --> (1.Name)@(2.domain).(3.extention)(.4.optional extention) --> raju@somecollege.edu.np
+ *  1.Letters, numbers, dots & hypens ; any length
+ *  2.Letters, numbers & hypens ; any length
+ *  3.Letters ; 2 to 8 character long
+ *  4.Letters ; 2 to 8 character long --> optional extention
+ */
+const EMAIL_REGEX = /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/
+
+/**
+ * Component
+ */
 const Register = () => {
+  // References
+  const userRef = useRef()
+  const errRef = useRef()
+
+  // State
+  const [user, setUser] = useState("")
+  const [validName, setValidName] = useState(false)
+
   return <div>Register</div>
 }
 
